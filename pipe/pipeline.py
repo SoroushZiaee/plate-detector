9764fbb23c5de891d01f62df10fd73f6a2c48a68
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2023-07-15 01:13:37 +0330

    get the first element of inference

--- pipe/pipeline.py
+++ pipe/pipeline.py
@@ -7,7 +7,7 @@ def inference_on_image(model, data_path: str):
     result_path = os.path.join(os.getcwd(), "results")
     os.makedirs(result_path, exist_ok=True)
 
-    results = model(data_path)
+    results = model(data_path)[0]
     detections = sv.Detections.from_yolov8(results)
     image = cv2.imread(data_path)
 
