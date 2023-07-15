ea116b256e6d538399f49236bfbc01b10e36dd10
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2023-07-15 11:40:09 +0330

    remove plate detection from character inference

--- pipe/pipeline.py
+++ pipe/pipeline.py
@@ -1,6 +1,15 @@
 import supervision as sv
+from supervision.video.dataclasses import VideoInfo
+from supervision.video.source import get_video_frames_generator
+
+from yolox.tracker.byte_tracker import BYTETracker
+from supervision.video.sink import VideoSink
+
+
 import os
 import cv2
+import numpy as np
+from dataclasses import dataclass
 
 from util.utils import preprocess_image
 
@@ -25,6 +34,13 @@ def inference_on_image(model, data_path: str, type_detection: str = "plate"):
         os.makedirs(result_path, exist_ok=True)
         results = model(data_path)[0]
         detections = sv.Detections.from_yolov8(results)
+
+        mask = np.array(
+            [class_id != 24 for class_id in detections.class_id], dtype=bool
+        )  # Remove plate detection
+
+        detections.filter(mask=mask, inplace=True)
+
         box_annotator = sv.BoxAnnotator()
         image = cv2.imread(data_path)
         with sv.ImageSink(target_dir_path=result_path, overwrite=True) as sink:
@@ -32,5 +48,22 @@ def inference_on_image(model, data_path: str, type_detection: str = "plate"):
             sink.save_image(image=annotated_frame)
 
 
+@dataclass(frozen=True)
+class BYTETrackerArgs:
+    track_thresh: float = 0.25
+    track_buffer: int = 30
+    match_thresh: float = 0.8
+    aspect_ratio_thresh: float = 3.0
+    min_box_area: float = 1.0
+    mot20: bool = False
+
+
 def inference_on_video(model, data_path):
+    byte_tracker = BYTETracker(BYTETrackerArgs())
+    video_info = VideoInfo.from_video_path(data_path)
+    print("\nvideo Info : ", end="")
+    print(video_info)
+
+    generator = get_video_frames_generator(conf["video_target_path"])
+
     raise NotImplementedError
