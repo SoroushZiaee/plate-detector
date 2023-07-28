750ef38159b0d3e04d73b9d06d632f4f7b79397d
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2023-07-28 12:04:36 +0330

    update model weights

--- model/load_model.py
+++ model/load_model.py
@@ -8,7 +8,7 @@ def load_model(model_path: str, type_detection: str):
         return YOLO(model_path)
 
     if type_detection == "character":
-        model_path = os.path.join(model_path, "character-detector.pt")
+        model_path = os.path.join(model_path, "best.pt")  # character-detector
         return YOLO(model_path)
 
     return None
