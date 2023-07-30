e9370b0c1bcc1aac5407acfffd0e6515de4b553b
Soroush Ziaee <32066123+SoroushZiaee@users.noreply.github.com>
2023-07-30 15:48:47 +0330

    edit model

--- model/load_model.py
+++ model/load_model.py
@@ -8,7 +8,7 @@ def load_model(model_path: str, type_detection: str):
         return YOLO(model_path)
 
     if type_detection == "type":
-        model_path = os.path.join(model_path, "type.pt")  # character-detector
+        model_path = os.path.join(model_path, "best.pt")  # character-detector
         return YOLO(model_path)
 
     if type_detection == "character":
